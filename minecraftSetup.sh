dc6444fbaff4b9fdeca7ab7c2f1c4ec5ed1c9498
JM-34771765 <34771765@student.murdoch.edu.au>
2025-06-05 11:24:03 +0800

    Update minecraftSetup.sh
    Removed startup launch

--- minecraftSetup.sh
+++ minecraftSetup.sh
@@ -32,12 +32,4 @@ printf '#!/bin/bash\nkill -9 $(ps -ef | pgrep -f "java")' >> stop
 chmod +x stop
 sleep 1
 
-# Create SystemD Script to run Minecraft server jar on reboot
-cd /etc/systemd/system/
-touch minecraft.service
-printf '[Unit]\nDescription=Minecraft Server on start up\nWants=network-online.target\n[Service]\nUser=minecraft\nWorkingDirectory=/opt/minecraft/server\nExecStart=/opt/minecraft/server/start\nStandardInput=null\n[Install]\nWantedBy=multi-user.target' >> minecraft.service
-sudo systemctl daemon-reload
-sudo systemctl enable minecraft.service
-sudo systemctl start minecraft.service
-
 # End script
