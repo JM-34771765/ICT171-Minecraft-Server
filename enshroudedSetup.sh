872a63a4f352fb1d23df9c5c1c1609b617f39752
JM-34771765 <34771765@student.murdoch.edu.au>
2025-06-08 22:25:20 +0800

    Update enshroudedSetup.sh

--- enshroudedSetup.sh
+++ enshroudedSetup.sh
@@ -14,7 +14,8 @@ sudo mkdir -pm755 /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/dists/$(lsb_release -cs)/winehq-$(lsb_release -cs).sources
 sudo dpkg --add-architecture i386
-sudo apt install --install-recommends winehq-staging
+sudo apt install wine32
+sudo apt install wine64
 sudo apt install cabextract winbind screen xvfb
 
 # Configure SteamCMD
